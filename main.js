e99aa6a8063db33e41511ef987b6fbbacf5890d1
emilysampaio07 <134975724+emilysampaio07@users.noreply.github.com>
2023-10-09 20:06:01 -0300

    h

--- main.js
+++ main.js
@@ -23,10 +23,10 @@ function tocaSomSplash () {
 }
 document.querySelector('.tecla_splash').onclick =tocaSomSplash;
   
-function tocaSomPomToim () {
+function tocaSomToim () {
   document.querySelector('#som_tecla_toim').play()
 }
-document.querySelector('.tecla_toim').onclick =tocaSomPomToim;
+document.querySelector('.tecla_toim').onclick =tocaSomToim;
   
 function tocaSomPsh () {
   document.querySelector('#som_tecla_psh').play()
