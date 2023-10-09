59fb8b738add51d9cba75afd78284af334c18344
emilysampaio07 <134975724+emilysampaio07@users.noreply.github.com>
2023-10-09 20:04:12 -0300

    f

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 function tocaSomPom () {
-  document.querySelector('#som_tleca_pom').play()
+  document.querySelector('#som_tecla_pom').play()
 }
 document.querySelector('.tecla_pom').onclick =tocaSomPom;
   
 function tocaSomClap () {
-  document.querySelector('#som_tleca_clap').play()
+  document.querySelector('#som_tecla_clap').play()
 }
 document.querySelector('.tecla_clap').onclick =tocaSomClap;
   
 function tocaSomTim () {
-  document.querySelector('#som_tleca_tim').play()
+  document.querySelector('#som_tecla_tim').play()
 }
 document.querySelector('.tecla_tim').onclick =tocaSomTim;
   
 function tocaSomPuff () {
-  document.querySelector('#som_tleca_puff').play()
+  document.querySelector('#som_tecla_puff').play()
 }
 document.querySelector('.tecla_puff').onclick =tocaSomPuff;
   
 function tocaSomSplash () {
-  document.querySelector('#som_tleca_splash').play()
+  document.querySelector('#som_tecla_splash').play()
 }
 document.querySelector('.tecla_splash').onclick =tocaSomSplash;
   
 function tocaSomPomToim () {
-  document.querySelector('#som_tleca_toim').play()
+  document.querySelector('#som_tecla_toim').play()
 }
 document.querySelector('.tecla_toim').onclick =tocaSomPomToim;
   
 function tocaSomPsh () {
-  document.querySelector('#som_tleca_psh').play()
+  document.querySelector('#som_tecla_psh').play()
 }
 document.querySelector('.tecla_psh').onclick =tocaSomPsh;
   
 function tocaSomTic () {
-  document.querySelector('#som_tleca_tic').play()
+  document.querySelector('#som_tecla_tic').play()
 }
 document.querySelector('.tecla_tic').onclick =tocaSomTic;
   
 function tocaSomTom () {
-  document.querySelector('#som_tleca_tom').play()
+  document.querySelector('#som_tecla_tom').play()
 }
 document.querySelector('.tecla_tom').onclick =tocaSomTom;
   
