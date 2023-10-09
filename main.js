aaf51c83be35017644a6d95fa69d813d7669f453
emilysampaio07 <134975724+emilysampaio07@users.noreply.github.com>
2023-10-09 19:30:27 -0300

    u

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function tocaSomPom () {
   document.querySelector('#som_tleca_pom').play()
 }
-document.querySelector('.tecla_pom').onclick =tocaSomPom();
+document.querySelector('.tecla_pom').onclick =tocaSomPom;
   
 
 
